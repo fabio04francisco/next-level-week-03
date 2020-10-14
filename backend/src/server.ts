d3f2faf53e099f77421c8189b362a7260e832328
Fabio Francisco <fabinhofrancisco@gmail.com>
2020-10-14 07:19:45 -0300

    Configurações do server.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,10 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
+app.use(express.json);
+
+app.get('/users', (request, response) => {
+  return response.json( {messagem: "Start backend"});
+});
 
 app.listen(3333);
