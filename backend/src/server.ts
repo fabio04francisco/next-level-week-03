68bb16fb634c87b7d04e7ec2defb0b3daa08e98b
Fabio Francisco <fabinhofrancisco@gmail.com>
2020-10-20 07:05:00 -0300

    Configuração cors.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 
 import 'express-async-errors';
 import './database/connection';
@@ -9,11 +10,10 @@ import errorHandler from './errors/randler';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
-
-
 
 app.listen(3333);
